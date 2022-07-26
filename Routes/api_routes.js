c65f7b888742eee6319f3b2f18ad172032232fbb
Kramclam <kennethramclam@gmail.com>
2022-07-26 12:34:35 -0400

    db setup and collections call

--- Routes/api_routes.js
+++ Routes/api_routes.js
@@ -2,8 +2,9 @@ import express from "express";
 import * as mongodb from "mongodb";
 import connectionString from "../Utility/dbaccesskey.js";
 
-const apiRouter = express.Router();
-export default apiRouter;
+const apiRouter_projects = express.Router();
+export const apiRouter_tasks = express.Router();
+export default apiRouter_projects;
 // personal access string used to connect to the database
 const accessString = connectionString;
 // declared this variable as the db client handler
@@ -21,14 +22,26 @@ MongoClient.connect(accessString, { useUnifiedTopology: true }).then(
      * 2. post tasks in task view
      *
      * */
-    apiRouter.get("/", (req, res) => {
+    apiRouter_projects.get("/", (req, res) => {
       // filter the destination
       projectsCollection
         .find()
         .toArray()
         .then((results) => {
-          console.log(results);
-          res.render("index.ejs", { projects: results });
+          //console.log(results);
+          res.render("projects", { projects: results });
+        })
+        .catch((error) => console.error(error));
+    });
+
+    apiRouter_tasks.get("/", (req, res) => {
+      // filter the destination
+      projectsCollection
+        .find()
+        .toArray()
+        .then((results) => {
+          //console.log(results);
+          res.render("tasks", { projects: results });
         })
         .catch((error) => console.error(error));
     });
